12d92a7e2a96eaf40fda107a2b55b7fd75307f3c
brook.blackburn <brook.blackburn@zirmed.com>
2016-09-22 13:38:07 -0400

    Fixed Analytics-5

--- PersonalWeb/PersonalWeb/App_Start/BundleConfig.cs
+++ PersonalWeb/PersonalWeb/App_Start/BundleConfig.cs
@@ -8,8 +8,14 @@ namespace PersonalWeb.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/bundles/css").Include("~/content/bootstrap.min.css", "~/content/Site.css"));
-            bundles.Add(new ScriptBundle("~/scripts").Include("~/scripts/ai.0.22.19-build00125.min.js", "~/scripts/bootstrap.min.js", "~/scripts/jquery-{version}.min.js","~/scripts/GoogleAnalytics.js"));
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include("~/content/bootstrap.min.css",
+                        "~/content/Site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .Include("~/scripts/ai.0.22.19-build00125.min.js",
+                        "~/scripts/bootstrap.min.js",
+                        "~/scripts/jquery-{version}.min.js",
+                        "~/scripts/GoogleAnalytics.js"));
         }
     }
 }
